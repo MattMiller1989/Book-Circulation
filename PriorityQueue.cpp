e227741e8855c7074b59a1ddc375c2c914d6fdc8
MattMiller1989 <57654274+MattMiller1989@users.noreply.github.com>
2020-07-02 11:45:58 -0500

    Update PriorityQueue.cpp
    Implemented selection sort for update().

--- PriorityQueue.cpp
+++ PriorityQueue.cpp
@@ -1,22 +1,54 @@
-
 #include "PriorityQueue.h"
-PriorityQueue::PriorityQueue() {}
+#include <iostream>
+using namespace std;
 
-void PriorityQueue::push(Employee* temp)
-{
-    
-}
 
-Employee* PriorityQueue::pop()
-{
-   
-}
-Employee* PriorityQueue::top() {
-    
-}
-bool PriorityQueue::empty() {
-   
-}
-void PriorityQueue::update() {
+
+template <typename type>
+void PriorityQueue<type>::push(type element) {
+	data.push_back(element);
+
+	
+	++size;
 
 }
+
+template <typename type>
+void PriorityQueue<type>::pop() {
+
+	if (size > 0) {
+		data.erase(data.begin());
+	}
+	else {
+		cout << "No more data in the queue" << endl;
+	}
+	
+}
+template <typename type>
+void PriorityQueue<type>::update() {		//sorts the items by a comparable value... in this case, priority
+	
+
+	for (int i = 0; i < data.size(); ++i) {
+		int pos_max = i;
+		for (int j = i + 1; j < data.size(); ++j) {   //uses selection sort @ O(n^2)
+			if (data[j] > data[pos_max])
+				pos_max = j;
+		}
+		if (pos_max != i) {
+			swap(data[i], data[pos_max]);
+		}
+	}
+
+}
+template <typename type>
+type* PriorityQueue<type>::front() {
+	return &data[0];
+}
+template <typename type>
+void PriorityQueue<type>::print_queue() {
+	for (int x = 0; x < data.size(); x++) {
+		cout << data[x] << " ";
+	}
+	cout << "\n";
+}
+
