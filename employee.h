a26591c3c042a2e6039c062fe04bcc093f96723f
MattMiller1989 <57654274+MattMiller1989@users.noreply.github.com>
2020-07-03 16:23:48 -0500

    Added methods for receiving and passing book

--- employee.h
+++ employee.h
@@ -16,6 +16,8 @@ public:
 	Employee(string new_name,int new_priority);
 	string get_name();
 	int priority();
+	void receive_book(int curr_wait);
+	void pass_book(int curr_retain);
 	friend bool operator<(Employee& lhs, Employee& rhs);
 	friend bool operator>(Employee& lhs, Employee& rhs);
 
