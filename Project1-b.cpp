5ef0fc969e838a0141d17f3482bd1090496a2f3e
MattMiller1989 <57654274+MattMiller1989@users.noreply.github.com>
2020-07-20 12:44:16 -0500

    Update Project1-b.cpp

--- Project1-b.cpp
+++ Project1-b.cpp
@@ -32,7 +32,14 @@ int main()
     library.pass_on("Software Engineering", Date(2015, 4, 10, DateFormat::US));
     library.pass_on("Software Engineering", Date(2015, 4, 15, DateFormat::US));
     library.print_employees();
-    
+
+    library.add_book("Underwater Basket Weaving");
+
+    library.circulate_book("Underwater Basket Weaving", Date(2015, 4, 16, DateFormat::US));
+    library.pass_on("Underwater Basket Weaving", Date(2015, 4, 17, DateFormat::US));
+    library.pass_on("Underwater Basket Weaving", Date(2015, 4, 18, DateFormat::US));
+    library.pass_on("Underwater Basket Weaving", Date(2015, 4, 24, DateFormat::US));
+    library.print_employees();
 
     return 0;
 }
